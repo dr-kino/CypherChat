f8bb05befed2a8c90ed11ad3e8ef4219aeecfecb
dr-kino <doctorkinoo@gmail.com>
2020-04-10 19:28:16 +0100

    Include unit tests with google test framework and deleted old test case, now covered by gtest.

--- src/cypher_lib.h
+++ src/cypher_lib.h
@@ -1,10 +1,20 @@
 #ifndef CYPHER_LIB_H
 #define CYPHER_LIB_H
 
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void DES_CBC ( int modo,
 		       char * bloco_entrada,
 		       char * chave,
                char * vetor,
 		       char * bloco_saida);
+			   
+#ifdef __cplusplus
+}
+#endif
 
 #endif // CYPHER_LIB_H
